b2041dc64f755ff969a12fd13aa515348d997bcd
woodwindfreak101 <woodwindfreak101@gmail.com>
2018-11-29 19:35:51 -0600

    Fixed and made FindByValue functional

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -63,7 +63,8 @@ namespace TechJobsConsole
                     // Fetch results
                     if (columnChoice.Equals("all"))
                     {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                        searchResults = JobData.FindByValue(searchTerm);
+                        PrintJobs(searchResults);
                     }
                     else
                     {
