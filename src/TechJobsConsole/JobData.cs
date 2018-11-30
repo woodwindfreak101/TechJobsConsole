843033c7e003d176003c50869983aa7791d868cb
woodwindfreak101 <woodwindfreak101@gmail.com>
2018-11-29 19:31:09 -0600

    Fixed FindByValue

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -38,32 +38,6 @@ namespace TechJobsConsole
             return values;
         }
 
-        public static List<Dictionary<string, string>> FindByValue(string valueJobs)
-        {
-            LoadData();
-
-            List<Dictionary<string, string>> valueJobs = new List<Dictionary<string, string>>();
-
-            foreach(Dictionary<string, string> job in AllJobs)
-            {
-                
-
-                foreach (KeyValuePair<string, string> detail in job)
-                {
-                    string detailValue = detail.Value;
-                    string detailKey = detail.Key;
-
-                    if(detailValue.ToUpper().Contains(searchValue.ToUpper()))
-                    {
-                        if(!valueJobs.Contains(job))
-                        {
-                            valueJobs.Add(job);
-                        }
-                    }
-                }
-            }
-            return valueJobs;
-        }
 
         public static List<Dictionary<string, string>> FindByColumnAndValue(string column, string value)
         {
@@ -164,6 +138,33 @@ namespace TechJobsConsole
             valueBuilder.Clear();
 
             return rowValues.ToArray();
+        }
+
+        public static List<Dictionary<string, string>> FindByValue(string searchValue)
+        {
+            LoadData();
+
+            List<Dictionary<string, string>> valueJobs = new List<Dictionary<string, string>>();
+
+            foreach (Dictionary<string, string> job in AllJobs)
+            {
+
+
+                foreach (KeyValuePair<string, string> detail in job)
+                {
+                    string detailValue = detail.Value;
+                    string detailKey = detail.Key;
+
+                    if (detailValue.ToUpper().Contains(searchValue.ToUpper()))
+                    {
+                        if (!valueJobs.Contains(job))
+                        {
+                            valueJobs.Add(job);
+                        }
+                    }
+                }
+            }
+            return valueJobs;
         }
     }
 }
